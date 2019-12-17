c1bfcba8ed149e294c12772ae354637fa3ad784d
XZiar <czktc2007@gmail.com>
2019-12-17 12:17:35 -0800

    Basic framework on Win32. Change to design of event + virtual event raise

--- Tests/UtilTest/WdHostTest.cpp
+++ Tests/UtilTest/WdHostTest.cpp
@@ -17,16 +17,19 @@ static MiniLogger<false>& log()
 static void WDHost()
 {
     const auto window = WindowHost_::Create();
-    window->OnOpen    += [](const auto&) { log().info(u"opened.\n"); };
-    window->OnClose   += [](const auto&) { log().info(u"closed.\n"); };
-    bool first = true;
-    window->OnDisplay += [&](const auto&) 
+    window->Openning    += [](const auto&) { log().info(u"opened.\n"); };
+    window->Closing     += [](const auto&) { log().info(u"closed.\n"); };
+    window->Displaying  += [first = true](const auto&) mutable
     {
         if (first)
             log().info(u"display.\n"); 
         first = false;
     };
-    window->Open();
+    window->Resizing    += [](const auto&, int32_t width, int32_t height)
+    {
+        log().info(u"resize to [{:4} x {:4}].\n", width, height);
+    };
+    window->Show();
     getchar();
     window->Close();
 }
